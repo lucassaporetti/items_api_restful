10500ef4494dbacc1d0e9d67e91c89230e74bf07
LucasSaporetti <lucassaporetti@gmail.com>
2020-07-09 12:05:18 -0300

    django ambience created

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/lucassaporetti/GIT-Repository/items_api_restful/venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
